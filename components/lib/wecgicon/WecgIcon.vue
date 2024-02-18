b2fe7570845bbd28f9202c641356aa03225e2ed5
Tony von Biedenfeld <tony.vonbiedenfeld@wagner-ecommerce.group>
2024-02-18 14:14:10 +0100

    setze Standardwerte für Stroke-Width & Farbe der Icons

--- components/lib/wecgicon/WecgIcon.vue
+++ components/lib/wecgicon/WecgIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg role="img" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" v-bind="pti()">
+    <svg role="img" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" :stroke-width="strokeWidth" :stroke="color" v-bind="pti()">
         <use :href="`${spritesheet}#svg.${icon}`"></use>
     </svg>
 </template>
@@ -33,6 +33,14 @@ export default {
         },
         icon: {
             type: String
+        },
+        strokeWidth: {
+            type: String,
+            default: '0.375rem'
+        },
+        color: {
+            type: String,
+            default: '#FFFFFF'
         }
     },
     methods: {
