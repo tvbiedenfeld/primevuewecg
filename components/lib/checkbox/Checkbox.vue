702b650208b69576a53a52694cdc2fe20921521f
Tony von Biedenfeld <tony.vonbiedenfeld@wagner-ecommerce.group>
2024-02-21 11:34:07 +0100

    stelle Checkbox von Text- auf SVG-Icon um

--- components/lib/checkbox/Checkbox.vue
+++ components/lib/checkbox/Checkbox.vue
@@ -21,7 +21,7 @@
         />
         <div :class="cx('box')" v-bind="getPTOptions('box')">
             <slot name="icon" :checked="checked" :class="cx('icon')">
-                <CheckIcon v-if="checked" :class="cx('icon')" v-bind="getPTOptions('icon')" />
+                <WecgIcon v-if="checked" :icon="check" :class="cx('iconClass')" v-bind="ptm('icon')" :label="icon" :size="size" :variant="variant" :disabled="disabled"></WecgIcon>
             </slot>
         </div>
     </div>
