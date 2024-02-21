dc4804293e3d5f0147c4a852a4b2470ed019d84b
Tony von Biedenfeld <tony.vonbiedenfeld@wagner-ecommerce.group>
2024-02-21 15:59:48 +0100

    mache Icon via "pt" konfigurierbar

--- components/lib/badge/Badge.vue
+++ components/lib/badge/Badge.vue
@@ -1,7 +1,7 @@
 <template>
     <span :class="cx('root')" :disabled="disabled" v-bind="getPTOptions('root')" :data-pc-variant="variant">
         <slot v-if="icon" name="icon" :class="[cx('icon')]">
-            <WecgIcon :icon="icon" :class="cx('iconClass')" v-bind="ptm('icon')" :label="icon" :size="size" :variant="variant" :disabled="disabled"></WecgIcon>
+            <WecgIcon :icon="icon" :class="cx('iconClass')" v-bind="getPTOptions('icon')" :label="icon" :size="size" :variant="variant" :disabled="disabled"></WecgIcon>
         </slot>
         <span :class="cx('label')" v-bind="ptm('label')">{{ label || '&nbsp;' }}</span>
     </span>
--- components/lib/button/Button.vue
+++ components/lib/button/Button.vue
@@ -2,10 +2,10 @@
     <button v-ripple :class="cx('root')" type="button" :aria-label="defaultAriaLabel" :disabled="disabled" v-bind="getPTOptions('root')" :data-p-severity="severity">
         <slot>
             <slot v-if="loading" name="loadingIcon" :class="[cx('loadingIcon'), cx('icon')]">
-                <WecgIcon icon="loading" :class="cx('iconClass')" v-bind="ptm('loadingIcon')" label="loading" :size="size" :variant="variant" :disabled="disabled" spin></WecgIcon>
+                <WecgIcon icon="loading" :class="cx('iconClass')" v-bind="getPTOptions('loadingIcon')" label="loading" :size="size" :variant="variant" :disabled="disabled" spin></WecgIcon>
             </slot>
             <slot v-else name="icon" :class="[cx('icon')]">
-                <WecgIcon v-if="icon" :icon="icon" :class="cx('iconClass')" v-bind="ptm('icon')" :label="icon" :size="size" :variant="variant" :disabled="disabled"></WecgIcon>
+                <WecgIcon v-if="icon" :icon="icon" :class="cx('iconClass')" v-bind="getPTOptions('icon')" :label="icon" :size="size" :variant="variant" :disabled="disabled"></WecgIcon>
             </slot>
             <span :class="cx('label')" v-bind="ptm('label')">{{ label || '&nbsp;' }}</span>
             <Badge v-if="badge" :value="badge" :class="badgeClass" :severity="badgeSeverity" :unstyled="unstyled" v-bind="ptm('badge')"></Badge>
