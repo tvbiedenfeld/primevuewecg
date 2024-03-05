3dfef5cff9aefd489c6d10a81ea1d09b786ffe1a
Tony von Biedenfeld <tony.vonbiedenfeld@wagner-ecommerce.group>
2024-03-05 15:08:28 +0100

    remove width and height from WecgIcon

--- components/lib/wecgicon/WecgIcon.vue
+++ components/lib/wecgicon/WecgIcon.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg role="img" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" :stroke-width="strokeWidth" :stroke="color" v-bind="pti()">
+    <svg role="img" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" :stroke-width="strokeWidth" :stroke="color" v-bind="pti()">
         <use :href="`${spritesheet}#svg.${icon}`"></use>
     </svg>
 </template>
